1a810c99e92e7bf43111b98dc230326e906513db
unknown <example@gmail.com>
2021-11-07 20:01:43 +0500

    Alhamdullilax

---
+++ src/Nav.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import "./nav.css";
+export default function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+        className="nav__logo"
+      />
+    </div>
+  );
+}
